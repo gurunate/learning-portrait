a9f61612504dc0a154691b85a3e634a3dd50b7f3
Nate Johnson <nathanalanjohnson@gmail.com>
2023-12-03 10:18:48 -0600

    wip

--- app/src/components/sign-in/index.tsx
+++ app/src/components/sign-in/index.tsx
@@ -1,4 +1,6 @@
-import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { Box, Card, CardContent, Grid, Stack, Typography } from '@mui/material';
+
+import Image from 'next/image';
 
 const SignIn = () => {
     return (
@@ -11,7 +13,29 @@ const SignIn = () => {
                 </Card>
             </Grid>
             <Grid item md={6}>
-                <Typography>content here</Typography>
+                <Stack direction="column" spacing={4}>
+                    <Image
+                        src="./logo-white.svg"
+                        width={231}
+                        height={136}
+                        alt="logo"
+                    />
+                    <Typography variant="h3" paragraph>
+                        Simple. Flexible. Empowering.
+                    </Typography>
+                    <Typography variant="subtitle1" paragraph>
+                        A grade book that thinks like you do.
+                    </Typography>
+                    <Typography paragraph>
+                        Learning Portrait was built from the ground up to think
+                        like, and support, the classrooms of the most
+                        progressive K-12 educators – allowing educators,
+                        students, families and school systems to finally monitor
+                        learning in a way that makes sense.  Students aren’t
+                        numbers. They are learners. See the portrait of what
+                        they can do.
+                    </Typography>
+                </Stack>
             </Grid>
         </Grid>
     );
