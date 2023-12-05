35d46388c78e516a40ff99996106a2e9c652eb2a
Nate Johnson <nathanalanjohnson@gmail.com>
2023-12-05 15:01:38 -0600

    wip

--- app/next.config.js
+++ app/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    ignorePatterns: ['*.stories.tsx', '*.stories.ts']
+    webpack: config => {
+        config.module.rules?.push({
+            test: /\\.stories\\.(tsx|ts|js|jsx)$/,
+            loader: 'ignore-loader'
+        });
+        return config;
+    }
 };
 
 module.exports = nextConfig;
