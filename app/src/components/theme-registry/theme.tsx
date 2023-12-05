36a7c2c3cb18b37531c1164c759fc6621b32665d
Nate Johnson <nathanalanjohnson@gmail.com>
2023-12-05 11:46:46 -0600

    wip

--- app/src/components/theme-registry/theme.tsx
+++ app/src/components/theme-registry/theme.tsx
@@ -1,4 +1,16 @@
+import { darken, lighten } from '@mui/system';
+
 import { createTheme } from '@mui/material/styles';
+
+// Color schema from Figma
+const PRIMARY_LP = '#006B96';
+const COLOR_3_ALERT = '#F75151';
+const COLOR_LIGHT_LP_2 = '#8DA97E';
+const COLOR_ADDITIONAL_BLUE1 = '#23ADDE';
+const COLOR_0_SECONDARY = '#F7941D';
+const COLOR_2_SUCCESS = '#009512';
+const LINE_LINE1 = '#DCDBDD';
+const LP_02_SECONDARY = '#3C5255';
 
 const BORDER_RADIUS = 12;
 
@@ -6,36 +18,36 @@ const theme = createTheme({
     palette: {
         mode: 'light',
         primary: {
-            light: '#008DC4',
-            main: '#006B96',
-            dark: '#005273'
+            light: lighten(PRIMARY_LP, 0.25),
+            main: PRIMARY_LP,
+            dark: darken(PRIMARY_LP, 0.25)
         },
         secondary: {
             light: '#D48019',
-            main: '#F7941D',
+            main: COLOR_0_SECONDARY,
             dark: '#59360B'
         },
         error: {
             // light: '#EF5350',
-            // main: '#F75151'
+            // main: COLOR_3_ALERT
             main: '#D44646'
             // dark: '#C62828'
+        },
+        warning: {
+            //     light: '#FF9800',
+            main: COLOR_0_SECONDARY
+            //     dark: '#C24400'
+        },
+        info: {
+            //     light: '#03A9F4',
+            main: COLOR_ADDITIONAL_BLUE1
+            //     dark: '#01579B'
+        },
+        success: {
+            //     light: '#4CAF50',
+            main: COLOR_2_SUCCESS
+            //     dark: '#1B5E20'
         }
-        // warning: {
-        //     light: '#FF9800',
-        //     main: '#ED6C02',
-        //     dark: '#C24400'
-        // },
-        // info: {
-        //     light: '#03A9F4',
-        //     main: '#0288D1',
-        //     dark: '#01579B'
-        // },
-        // success: {
-        //     light: '#4CAF50',
-        //     main: '#2E7D32',
-        //     dark: '#1B5E20'
-        // },
     },
     typography: {
         fontFamily: ['"Poppins"', 'sans-serif'].join(','),
@@ -104,7 +116,7 @@ const theme = createTheme({
             styleOverrides: {
                 root: {
                     fontWeight: 700,
-                    borderColor: '#DCDBDD',
+                    borderColor: LINE_LINE1,
                     borderRadius: BORDER_RADIUS
                 }
             }
@@ -117,7 +129,7 @@ const theme = createTheme({
         MuiSvgIcon: {
             styleOverrides: {
                 root: {
-                    color: '#3C5255'
+                    color: LP_02_SECONDARY
                 }
             }
         },
