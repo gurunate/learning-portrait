544834f0985cc80edf5778126c48a4203654625f
Al Bonney <32958954+albonney@users.noreply.github.com>
2024-05-04 16:56:32 +0000

    Wrap seed creates in await Promise.all()

--- api/prisma/seed/index.mjs
+++ api/prisma/seed/index.mjs
@@ -10,90 +10,102 @@ const prisma = new PrismaClient();
 
 const seedData = async () => {
     // Users
-    users.map(user =>
-        prisma.user.upsert({
-            where: { id: user?.id },
-            update: {},
-            create: user
-        })
+    await Promise.all(
+        users.map(async user =>
+            prisma.user.upsert({
+                where: { id: user?.id },
+                update: {},
+                create: user
+            })
+        )
     );
 
     const updatedById = users[0]?.id;
     const createdById = updatedById;
 
     // Instructors
-    instructors.map(async instructor =>
-        prisma.instructor.upsert({
-            where: { id: instructor?.id },
-            update: instructor,
-            create: {
-                ...instructor,
-                updatedById,
-                createdById
-            }
-        })
+    await Promise.all(
+        instructors.map(async (instructor) =>
+            prisma.instructor.upsert({
+                where: { id: instructor?.id },
+                update: instructor,
+                create: {
+                    ...instructor,
+                    updatedById,
+                    createdById
+                }
+            })
+        )
     );
 
     // Courses
-    courses.map(async course =>
-        prisma.course.upsert({
-            where: { id: course?.id },
-            update: course,
-            create: {
-                ...course,
-                updatedById,
-                createdById
-            }
-        })
+    await Promise.all(
+        courses.map(async (course) =>
+            prisma.course.upsert({
+                where: { id: course?.id },
+                update: course,
+                create: {
+                    ...course,
+                    updatedById,
+                    createdById
+                }
+            })
+        )
     );
 
     // Sections
-    sections.map(async section =>
-        prisma.section.upsert({
-            where: { id: section?.id },
-            update: section,
-            create: {
-                ...section,
-                updatedById,
-                createdById
-            }
-        })
+    await Promise.all(
+        sections.map(async (section) =>
+            prisma.section.upsert({
+                where: { id: section?.id },
+                update: section,
+                create: {
+                    ...section,
+                    updatedById,
+                    createdById
+                }
+            })
+        )
     );
 
     // Ratings
-    ratings.map(async rating =>
-        prisma.rating.upsert({
-            where: { id: rating?.id },
-            update: rating,
-            create: {
-                ...rating,
-                updatedById,
-                createdById
-            }
-        })
+    await Promise.all(
+        ratings.map(async (rating) =>
+            prisma.rating.upsert({
+                where: { id: rating?.id },
+                update: rating,
+                create: {
+                    ...rating,
+                    updatedById,
+                    createdById
+                }
+            })
+        )
     );
 
     // Objectives
-    objectives.map(async objective =>
-        prisma.objective.upsert({
-            where: { id: objective?.id },
-            update: objective,
-            create: {
-                ...objective,
-                updatedById,
-                createdById
-            }
-        })
+    await Promise.all(
+        objectives.map(async (objective) =>
+            prisma.objective.upsert({
+                where: { id: objective?.id },
+                update: objective,
+                create: {
+                    ...objective,
+                    updatedById,
+                    createdById
+                }
+            })
+        )
     );
-
-    await prisma.$disconnect();
 };
 
 seedData()
-    .catch(e => {
+    .catch(async (e) => {
+        await prisma.$disconnect();
         console.error(`Error seeding: ${e}`);
         process.exit(1);
     })
     .finally(async () => {
+        await prisma.$disconnect();
         console.log('Seeding successfully.');
     });
