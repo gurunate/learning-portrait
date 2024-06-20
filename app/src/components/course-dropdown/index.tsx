43cc1d27f660e9abfa741f91dd0aba873bb301a7
hanna littlefield <hannamlittlefield@gmail.com>
2024-06-20 14:17:05 -0400

    Creating course dropdown

--- app/src/components/course-dropdown/index.tsx
+++ app/src/components/course-dropdown/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Autocomplete, FormControl, Select, TextField } from "@mui/material";
+import {FormControl, MenuItem, Select, SelectChangeEvent} from "@mui/material";
 
 import React from "react";
 import { Course as TCourse } from '@/types';
@@ -9,35 +9,37 @@ export type CourseSelectProps ={
     courses: TCourse[];
 }
 
+
 const CourseSelect: React.FC<CourseSelectProps> = ({
     courses = []
 }: CourseSelectProps): JSX.Element => {
+    const [course, setCourse] = React.useState('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+      setCourse(event.target.value as string);
+    };
 
 return(
     <>
-<Autocomplete
-  disablePortal
-  id="course-label"
-  options={courses[0]?.id}
-  sx={{ width: 300 }}
-  freeSolo
-  renderInput={(params) => <TextField {...params} label="Course" />}
-/>
-
-
 <FormControl fullWidth>
 <Select
     labelId="course-label"
     id="course"
-    value={courses[0]?.id}
+    value={course}
     aria-label="Courses"
+    onChange={handleChange}
+    sx={{borderRadius: '4px', border: '1px solid #D5D9DB', width: '273px'}}
 >
-    {courses.map(({ id, name }) => (
-        <MenuItem key={id} value={id}>
-            {name}
-        </MenuItem>
-    ))}
+    {courses
+        .slice()
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map(({ id, name }) => (
+    <MenuItem key={id} value={id}>
+      {name}
+    </MenuItem>
+  ))}
 </Select>
 </FormControl>
 </>);
 };
+ export default CourseSelect
