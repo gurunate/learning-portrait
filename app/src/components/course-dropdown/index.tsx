6b9b9a9b3afb227ca621cadbe537fb024f9a0815
hanna littlefield <hannamlittlefield@gmail.com>
2024-06-25 09:49:36 -0400

    Formatting code with Prettier

--- app/src/components/course-dropdown/index.tsx
+++ app/src/components/course-dropdown/index.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import {FormControl, MenuItem, Select, SelectChangeEvent} from "@mui/material";
+import {
+    FormControl,
+    MenuItem,
+    Select,
+    SelectChangeEvent
+} from '@mui/material';
 
-import React from "react";
+import React from 'react';
 import { Course as TCourse } from '@/types';
 
-export type CourseSelectProps ={
+export type CourseSelectProps = {
     courses: TCourse[];
-}
-
+};
 
 const CourseSelect: React.FC<CourseSelectProps> = ({
     courses = []
@@ -16,30 +20,35 @@ const CourseSelect: React.FC<CourseSelectProps> = ({
     const [course, setCourse] = React.useState('');
 
     const handleChange = (event: SelectChangeEvent) => {
-      setCourse(event.target.value as string);
+        setCourse(event.target.value as string);
     };
 
-return(
-    <>
-<FormControl fullWidth>
-<Select
-    labelId="course-label"
-    id="course"
-    value={course}
-    aria-label="Courses"
-    onChange={handleChange}
-    sx={{borderRadius: '4px', border: '1px solid #D5D9DB', width: '273px'}}
->
-    {courses
-        .slice()
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .map(({ id, name }) => (
-    <MenuItem key={id} value={id}>
-      {name}
-    </MenuItem>
-  ))}
-</Select>
-</FormControl>
-</>);
+    return (
+        <>
+            <FormControl fullWidth>
+                <Select
+                    labelId="course-label"
+                    id="course"
+                    value={course}
+                    aria-label="Courses"
+                    onChange={handleChange}
+                    sx={{
+                        borderRadius: '4px',
+                        border: '1px solid #D5D9DB',
+                        width: '273px'
+                    }}
+                >
+                    {courses
+                        .slice()
+                        .sort((a, b) => a.name.localeCompare(b.name))
+                        .map(({ id, name }) => (
+                            <MenuItem key={id} value={id}>
+                                {name}
+                            </MenuItem>
+                        ))}
+                </Select>
+            </FormControl>
+        </>
+    );
 };
- export default CourseSelect
+export default CourseSelect;
