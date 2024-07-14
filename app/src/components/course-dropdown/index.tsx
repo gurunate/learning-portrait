1f75dc463412839dff28b6398b0797288e584234
Daniel Vigil <daniel.julian.vigil@gmail.com>
2024-07-14 13:44:12 -0400

    #143 adding section to course dropdown

--- app/src/components/course-dropdown/index.tsx
+++ app/src/components/course-dropdown/index.tsx
@@ -2,9 +2,11 @@
 
 import {
     FormControl,
+    ListSubheader,
     MenuItem,
     Select,
     SelectChangeEvent,
+    Typography,
 } from '@mui/material';
 
 import React from 'react';
@@ -12,8 +14,8 @@ import { Course as TCourse } from '@/types';
 
 export type CourseSelectProps = {
     courses: TCourse[];
-    onHandleChange: (event: SelectChangeEvent) => void;
-    value: string;
+    onHandleChange?: (event: SelectChangeEvent) => void;
+    value?: string;
 };
 
 const CourseDropdown: React.FC<CourseSelectProps> = ({
@@ -38,10 +40,17 @@ const CourseDropdown: React.FC<CourseSelectProps> = ({
                     {courses
                         .slice()
                         .sort((a, b) => a.name.localeCompare(b.name))
-                        .map(({ id, name }) => (
-                            <MenuItem key={id} value={id}>
-                                {name}
-                            </MenuItem>
+                        .map(({id, name, sections }) => (
+                            <>
+                                <ListSubheader key={id}>
+                                    <Typography variant='body1' sx={{ fontWeight: 'bold' }}>{name}</Typography>
+                                </ListSubheader>
+                                {sections.map(({ id, name }) => (
+                                    <MenuItem key={id} value={id} sx={{ paddingInlineStart: 5 }}>
+                                        <Typography variant='body1'>{name}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </>
                         ))}
                 </Select>
             </FormControl>
