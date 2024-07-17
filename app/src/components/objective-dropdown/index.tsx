55df9e2c7c656eacacc3bb84b7fadc1a36c58b99
hanna littlefield <hannamlittlefield@gmail.com>
2024-07-16 20:46:36 -0400

    WIP

--- app/src/components/objective-dropdown/index.tsx
+++ app/src/components/objective-dropdown/index.tsx
@@ -6,6 +6,7 @@ import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
 import Checkbox from '@mui/material/Checkbox';
 import { Objective as TObjective } from '@/types';
 import TextField from '@mui/material/TextField';
+import { objectives } from '@/lib/fixtures'
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" sx={{ backgroundColor: "primary" }} />;
@@ -18,6 +19,8 @@ export type ObjectivesDropdownProps = {
     onHandleInputChange?: (event: any, value: string) => void;
 }
 
+const subObjectives = objectives(3);
+
 const ObjectivesDropdown: React.FC<ObjectivesDropdownProps> = ({
     objectives = [],
     value,
@@ -25,11 +28,14 @@ const ObjectivesDropdown: React.FC<ObjectivesDropdownProps> = ({
     onHandleChange,
     onHandleInputChange
   }) => {
+    {console.log(objectives)}
+    console.log(subObjectives)
       
       return (
         <Autocomplete
           multiple
           id="objectives-dropdown"
+          limitTags={2}
           options={objectives}
           onChange={onHandleChange}
           value={value}
